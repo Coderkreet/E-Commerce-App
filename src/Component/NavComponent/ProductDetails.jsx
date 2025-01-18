21643d3d7ec2afd3fd448896adaae55f62b9345c
Coderkreet <krunalbende2@gmail.com>
2025-01-18 23:26:04 +0530

    change

--- src/Component/NavComponent/ProductDetails.jsx
+++ src/Component/NavComponent/ProductDetails.jsx
@@ -70,12 +70,12 @@ const ProductDetails = ({ setCart, cart,IsLogin,seAmount }) => {
           <p className="text-gray-600 mb-4">Stock: {product.stock}</p>
           <div className="flex space-x-4">
             <button
-              onClick={()=>{
+              onClick={
                 IsLogin?
                 handleAddToCart :
                 toast.error("First You Need to Login")
-                Navigation('/Login')
-              }}
+                
+              }
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             >
               Add to Cart
